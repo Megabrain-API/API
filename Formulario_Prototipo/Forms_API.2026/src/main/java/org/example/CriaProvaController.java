de0e8427c53e994aa376db0d3ed4e52ed98af09d
Rian Fernandes <rrianpaes@gmail.com>
2026-09-16 22:28:59 -0300

    Editor de Formulario - Inserindo a pagina CriaProv
    Foi adicionado ao projeto de formulario dinamico a pagina de Informações da prova (Nome da prova, disciplona etc) e as telas foram interligadas
    
    Signed-off-by: Rian Fernandes <rrianpaes@gmail.com>

--- Formulario_Prototipo/Forms_API.2026/src/main/java/org/example/CriaProvaController.java
+++ Formulario_Prototipo/Forms_API.2026/src/main/java/org/example/CriaProvaController.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class CriaProvaController {
+
+    @FXML
+    private Button confirmCriarProva;
+
+    @FXML
+    private TextField nomeProva;
+
+    @FXML
+    private TextField prazoEntrega;
+
+    @FXML
+    private ComboBox<?> selectDisciplina;
+
+    @FXML
+    private ComboBox<?> selectQuestoes;
+
+    @FXML
+    void confirmCriarProva(ActionEvent event) throws IOException {
+        App.setRoot("FormEditor");
+    }
+
 }
