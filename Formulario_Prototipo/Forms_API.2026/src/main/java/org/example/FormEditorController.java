3408c1ddd3389531838453b0c4a13b6e36296e62
1cafee <yagonoronhachaves@gmail.com>
2026-09-16 22:47:32 -0300

    Editor de Formularios - Células de Questões
    Foi adicionado a possibilidade de gerar novas alternativas (atualmente sem checkbox) para cada questão e também remover as alternativas.
    
    Signed-off-by: 1cafee <yagonoronhachaves@gmail.com>

--- Formulario_Prototipo/Forms_API.2026/src/main/java/org/example/FormEditorController.java
+++ Formulario_Prototipo/Forms_API.2026/src/main/java/org/example/FormEditorController.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -22,15 +23,38 @@ public class FormEditorController {
         // Campo de texto para a pergunta
         TextField txtPergunta = new TextField();
         txtPergunta.setPromptText("Digite sua nova pergunta...");
+        TextField txtAlternativa = new TextField();
+        txtAlternativa.setPromptText("Digite a Alternativa...");
+
 
         // Botão para remover esta questão específica (opcional, igual ao Forms)
         Button btnRemover = new Button("Remover");
         btnRemover.setOnAction(e -> container_dinamico.getChildren().remove(novaQuestaoBox));
 
+
+
+        // botao pra adicionar alternativa na questao
+        Button btnAddAlt = new Button("Adicionar Alternativa");
+        btnAddAlt.setOnAction(a -> {
+
+            TextField novo = new TextField();
+            novo.setPromptText("digite alternativa");
+            novaQuestaoBox.getChildren().add(novo);
+
+            Button btnRemoveralt = new Button("Remover Alternativa");
+            btnRemoveralt.setOnAction(e -> {
+                novaQuestaoBox.getChildren().remove(novo);
+
+                novaQuestaoBox.getChildren().remove(btnRemoveralt);
+            });
+            novaQuestaoBox.getChildren().add(btnRemoveralt);
+        });
+
         // Adiciona os elementos dentro do bloco da questão
-        novaQuestaoBox.getChildren().addAll(txtPergunta, btnRemover);
+        novaQuestaoBox.getChildren().addAll(txtPergunta, btnRemover, btnAddAlt);
 
         // Adiciona o bloco da questão no container principal da tela
         container_dinamico.getChildren().add(novaQuestaoBox);
     }
+    void adAlternativa(ActionEvent event){}
 }
